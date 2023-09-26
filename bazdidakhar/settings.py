ae50b3bcb71c0ce408b598aba1d742dba8186cd6
MahdiHabibiXr <themahdihabibi@gmail.com>
2023-09-26 14:42:17 +0330

    update

--- bazdidakhar/settings.py
+++ bazdidakhar/settings.py
@@ -96,13 +96,22 @@ WSGI_APPLICATION = 'bazdidakhar.wsgi.application'
 #         "PORT": os.getenv("DB_PORT"),
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
